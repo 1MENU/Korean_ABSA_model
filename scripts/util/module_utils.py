e1fb5c42e231e0e7d32cb2abe1fa6227c7155e40
juicyjung <travelandi01@gmail.com>
2022-09-20 20:57:36 +0900

    json list concat (by Doyeon)

--- scripts/util/module_utils.py
+++ scripts/util/module_utils.py
@@ -5,9 +5,18 @@ import torch
 import numpy as np
 import random
 from torch import nn
+import os
 
 import copy
 
+from util.utils import *
+
+
+
+if not os.path.exists("../saved_model/category_extraction/"):
+    os.makedirs("../saved_model/category_extraction/")
+if not os.path.exists("../saved_model/polarity_classification/"):
+    os.makedirs("../saved_model/polarity_classification/")
 
 def set_seed(seedNum, device):
     torch.manual_seed(seedNum)
@@ -146,3 +155,92 @@ def evaluation_f1(true_data, pred_data):
         'category extraction result': ce_result,
         'entire pipeline result': pipeline_result
     }
+
+
+homePth = getParentPath(os.getcwd())
+datasetPth = homePth + '/dataset/'
+
+saveDirPth_str = homePth + '/materials/model_save/'
+predPth = homePth + '/materials/pred/'
+submissionPth = homePth + '/materials/submission/'
+
+
+from sklearn.model_selection import StratifiedKFold
+
+def stratified_KFold(file_list, n_splits, which_k, label_name):
+
+    skf = StratifiedKFold(n_splits = n_splits, shuffle = True)
+
+    data = pd.DataFrame()
+    
+    for data_file in file_list:
+        data = pd.concat([data, pd.read_csv(os.path.join(datasetPth, data_file), sep="\t")])
+
+    features = data.iloc[:,:]
+
+    label = data[label_name]
+
+    n_iter = 0
+
+    for train_idx, test_idx in skf.split(features, label):
+        n_iter += 1
+
+        # label_train = label.iloc[train_idx]
+        # label_test = label.iloc[test_idx]
+
+        features_train = features.iloc[train_idx]
+        features_test = features.iloc[test_idx]
+
+        if n_iter == which_k:
+            print(f'------------------ {n_splits}-Fold 중 {n_iter}번째 ------------------')
+
+            # print(features_test)
+
+            return features_train, features_test
+
+
+# jsonl 파일 읽어서 list에 저장
+def jsonlload(fname_list, encoding="utf-8"):
+    json_list = []
+
+    for index, value in enumerate(fname_list):
+        fname = "../dataset/" + value
+
+        with open(fname, encoding=encoding) as f:
+            for line in f.readlines():
+                json_list.append(json.loads(line))
+
+    return json_list
+
+
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+def load_TSNE(out, y_true):
+    tsne_np = TSNE(n_components = 2).fit_transform(out)
+
+    tsne_df = pd.DataFrame(tsne_np, columns = ['component 0', 'component 1'])
+
+    tsne_df['target'] = y_true
+
+    tsne_df_0 = tsne_df[tsne_df['target'] == 0]
+    tsne_df_1 = tsne_df[tsne_df['target'] == 1]
+
+    area = 2**2
+
+    plt.scatter(tsne_df_0['component 0'], tsne_df_0['component 1'], s = area, color = 'pink', label = 'setosa')
+    plt.scatter(tsne_df_1['component 0'], tsne_df_1['component 1'], s = area, color = 'purple', label = 'versicolor')
+
+    plt.xlabel('component 0')
+    plt.ylabel('component 1')
+    plt.legend()
+
+    plt.savefig('boston.png')
+
+def softmax(x):
+    
+    max = np.max(x,axis=1,keepdims=True) #returns max of each row and keeps same dims
+    e_x = np.exp(x - max) #subtracts each row with its max value
+    sum = np.sum(e_x,axis=1,keepdims=True) #returns sum of each row and keeps same dims
+    f_x = e_x / sum 
+    return f_x
