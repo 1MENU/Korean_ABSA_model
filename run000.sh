5f76b5b8348a48adf544309c3958df5164d26bb9
juicyjung <travelandi01@gmail.com>
2022-10-17 13:58:06 -0400

    original

--- run000.sh
+++ run000.sh
@@ -14,8 +14,10 @@ cd scripts
 
 # CUDA_VISIBLE_DEVICES=0
 
+# --nsplit=3 --kfold=1
+
 
 run_task 1 CD
 CUDA_VISIBLE_DEVICES=0 python CD_pipeline.py --name "1lay_90" \
-    --batch_size=64 --lr=1e-5 --pretrained="kykim/electra-kor-base" \
-    --LS=0.01 --weight_decay=0.001 --seed=2 --save=1 --nsplit=3 --kfold=2
+    --batch_size=64 --lr=2e-5 --pretrained="kykim/electra-kor-base" \
+    --LS=0.00 --weight_decay=0.001 --seed=3 --save=1 --nsplit=3 --kfold=3
